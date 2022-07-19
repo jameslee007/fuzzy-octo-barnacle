b15ee98a039903586d384f6e1f587cba95fa8270
Ahren Stevens-Taylor <ahrent@packt.com>
2022-07-19 18:56:40 +0000

    feat: add run.sh to outline the commands to run the service

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+## Install the dependencies
+poetry install
+
+## Run the service
+### Install the Meltano dependencies
+poetry run meltano install
+
+### Run the scheduler
+poetry run meltano invoke airflow scheduler
+
+### Run the Webserver
+poetry run meltano invoke webserver
